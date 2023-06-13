913c29fa4f9bb8fde208c22155ddfeeee7ff6075
Adithya <adithyavibakar@gmail.com>
2023-06-12 23:50:43 -0400

    final commit

--- src/tuiter/user/profile-screen.js
+++ src/tuiter/user/profile-screen.js
@@ -17,7 +17,7 @@ function ProfileScreen() {
      const { payload } = await dispatch(profileThunk());
      setProfile(payload);
    };
-   loadProfile()
+   loadProfile();
  }, [ ]);
  
  return ( <div>
